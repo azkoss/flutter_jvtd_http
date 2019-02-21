911716cc0bed6ad298a41876a57c65e5da31d472
陈磊的MacBook Pro <200892907@qq.com>
2019-02-21 10:40:09 +0800

    提升版本号

--- lib/src/jvtd_http_config.dart
+++ lib/src/jvtd_http_config.dart
@@ -2,8 +2,10 @@ import 'package:dio/dio.dart';
 import 'dart:io';
 
 /// 全局使用的[Dio]请求对象
-Dio dio = Dio(BaseOptions(
-  connectTimeout: 30000,
-  receiveTimeout: 30000,
-  contentType: ContentType.parse("application/x-www-form-urlencoded"),
-));
+Dio dio = Dio(
+  BaseOptions(
+    connectTimeout: 30000,
+    receiveTimeout: 30000,
+    contentType: ContentType.parse("application/x-www-form-urlencoded"),
+  ),
+);
